6b8ca5f8310a1bcd56493243d75b139039166768
Mark McDonald and Roberto Ostinelli <pair+mmcdonald+roberto@pivotallabs.com>
2014-02-12 16:44:15 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.14.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sup::Application.config.secret_key_base = '0cf1c5dd4f5b9bd26d7d310fc992c6443574d030f8e560fc0d5e52935aa7decda19a61fa7afe8be39b9b7f71551776a1a5b5f12d54e442257704dc159f34c096'
